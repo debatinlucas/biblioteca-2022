aa7962237caec60726b79bdf9b2303ada81b3d56
Lucas Debatin <lucasdebatin@univali.br>
2022-11-04 11:07:58 -0300

    CM - change expires JWT

--- biblioteca-back/auth/auth_handler.py
+++ biblioteca-back/auth/auth_handler.py
@@ -14,7 +14,7 @@ def token_response(token: str):
 def signJWT(user_id: str):
     payload = {
         "user_id": user_id,
-        "expires": time.time() + 600
+        "expires": time.time() + 60 * 60 * 60
     }
     token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
 
