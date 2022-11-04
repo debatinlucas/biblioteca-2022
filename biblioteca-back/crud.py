14e85ce7ab5f89cc5eb72a3095b71b39496612fe
Lucas Debatin <lucasdebatin@univali.br>
2022-11-03 21:23:26 -0300

    CM: add bcrypt

--- biblioteca-back/crud.py
+++ biblioteca-back/crud.py
@@ -2,15 +2,15 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import and_
 from exceptions import UsuarioAlreadyExistError, UsuarioNotFoundError, EmprestimoNotFoundError, LivroNotFoundError, ItemEmprestimoAlreadyExistError, ItemEmprestimoNotFoundError
-import models, schemas
+import bcrypt, models, schemas
 
 # usuário
 
 def check_usuario(db: Session, usuario: schemas.UsuarioLoginSchema):
-    db_usuario = db.query(models.Usuario).filter(and_(models.Usuario.email == usuario.email, models.Usuario.senha == usuario.senha)).first()
+    db_usuario = db.query(models.Usuario).filter(models.Usuario.email == usuario.email).first()
     if db_usuario is None:
         return False
-    return True
+    return bcrypt.checkpw(usuario.senha.encode('utf8'), db_usuario.senha.encode('utf8'))
 
 def get_usuario_by_id(db: Session, usuario_id: int):
     db_usuario = db.query(models.Usuario).get(usuario_id)
@@ -26,6 +26,8 @@ def get_usuario_by_email(db: Session, usuario_email: str):
 
 def create_usuario(db: Session, usuario: schemas.UsuarioCreate):
     db_usuario = get_usuario_by_email(db, usuario.email)
+    # O parâmetro rounds do gensalt determina a complexidade. O padrão é 12.
+    usuario.senha = bcrypt.hashpw(usuario.senha.encode('utf8'), bcrypt.gensalt())
     if db_usuario is not None:
         raise UsuarioAlreadyExistError
     db_usuario = models.Usuario(**usuario.dict())
@@ -38,8 +40,9 @@ def update_usuario(db: Session, usuario_id: int, usuario: schemas.UsuarioCreate)
     db_usuario = get_usuario_by_id(db, usuario_id)
     db_usuario.nome = usuario.nome
     db_usuario.email = usuario.email
-    if usuario.senha is not "":
-        db_usuario.senha = usuario.senha
+    if usuario.senha != "":
+        # O parâmetro rounds do gensalt determina a complexidade. O padrão é 12.
+        db_usuario.senha = bcrypt.hashpw(usuario.senha.encode('utf8'), bcrypt.gensalt())
     db.commit()
     db.refresh(db_usuario)
     return db_usuario
